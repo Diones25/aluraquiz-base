bb7558887dd922761a814c66f64e2618cb8642ac
Diones Pereira Alves <pereiradiones987@gmail.com>
2021-02-04 02:16:01 -0300

    Adicionando o Quiz

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground' 
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -14,37 +20,62 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
-  return(
+  const router = useRouter();
+  const [name, setName] = useState('');
+  console.log(name);
+
+  return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-            <Widget.Header>
-              <h1>Iron Man</h1>
-            </Widget.Header>
+          <Widget.Header>
+            <h1>Iron Man</h1>
+          </Widget.Header>
 
-            <Widget.Content>
-              <p>
-                Lorem ipsum dolor sit amet...
-              </p>
-            </Widget.Content>
+          <Widget.Content>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(info) => {
+                  setName(info.target.value);
+                }}
+                placeholder="Diz ai seu nome para jogar ;)"
+                value={name}
+              />
+
+              <Button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </Button>
+            </form>
+          </Widget.Content>
         </Widget>
 
         <Widget>
-            <Widget.Header>
-              <h1>Iron Man</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>
-                Lorem ipsum dolor sit amet...
-              </p>
-            </Widget.Content>
+          <Widget.Header>
+            <h1>Iron Man</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor sit amet...
+            </p>
+          </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/Diones25"/>
+      <GitHubCorner projectUrl="https://aluraquiz-base.diones25.vercel.app/"/>
     </QuizBackground>
-  )
+  );
 }
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid ${({theme}) => theme.colors.primary};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
   background-color: #1c1814;
   border-radius: 4px;
   overflow: hidden;
@@ -18,19 +18,19 @@ const Widget = styled.div`
     font-weight: 700;
     line-height: 1;
   }
-`
+`;
 
 Widget.Header = styled.header`
   display: flex;
   justify-content: flex-start;
   align-items: center;
   padding: 18px 32px;
-  background-color: ${({theme}) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 
   *{
     margin: 0;
   }
-`
+`;
 
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
@@ -44,5 +44,24 @@ Widget.Content = styled.div`
     list-style: none;
     padding: 0;
   }
-`
-export default Widget
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+
+  &:hover,
+  &:focus{
+    opacity: .5s;
+  }
+`;
+
+export default Widget;
